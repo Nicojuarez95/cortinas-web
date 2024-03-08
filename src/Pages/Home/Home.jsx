71f137b28c46b97a880cd9ff4e81ea3b6d97ac5a
Nicolas Juarez <juareznico61@gmail.com>
2024-03-08 10:59:23 -0300

    web cortinas

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -14,6 +14,169 @@ export default function Home() {
           <p className='resenia'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla doloremque nobis quia porro quibusdam blanditiis numquam, facere quas excepturi commodi nisi dolore iste unde quasi voluptatem quod facilis fugit et!Lorem Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse maxime maiores porro alias non, ea molestiae beatae distinctio sint iure totam delectus necessitatibus sit culpa exercitationem eveniet deserunt voluptatibus id? Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim non est minus minima quo eligendi, voluptas praesentium numquam mollitia dolore quidem facere ut quae! Ab quod doloribus deleniti consequuntur ad! Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus est soluta consequatur maiores fugit repellat quisquam sunt magni corrupti, quae fuga similique, voluptates quidem modi voluptatum id at laudantium velit? Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates quas impedit incidunt nostrum ipsa id odio quaerat voluptate quia, delectus labore ad laudantium voluptatibus recusandae. Fuga non ex libero dicta!</p>
         </div>
       </div>
+
+      <div className="catalogo">
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        <div className="categoriaCortina">
+            <div className='imgCate'>
+              <img src={logo} alt="" />
+            </div>
+          <button>VER MAS</button>
+        </div>
+        
+      </div>
+
+      <div className="productosCont">
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+        <div className="producto">
+          <img src={logo} alt="" />
+          <div className="textProducto">
+            <h2>PRODUCTO</h2>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Soluta tempora quasi velit harum cupiditate, quae distinctio eveniet itaque, repellat iusto, cum hic. Quos eius praesentium tempore perferendis, obcaecati beatae sequi.
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, cumque sequi. Perferendis, libero sequi! Cumque accusantium voluptates expedita nisi consectetur reprehenderit ratione saepe, praesentium, assumenda deserunt iure fugiat aliquid quo? Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit quis reprehenderit, quia minima obcaecati aliquid est cumque, eius cupiditate porro nesciunt magnam quo, facilis consequatur ullam maxime quasi voluptas eveniet.
+            </p>
+          </div>
+          <div className="contbutton">
+            <button>Mas Info</button>
+          </div>
+        </div>
+      </div>
+
+      <div className="clientesCont">
+          <div className="cliente">
+            <img src={logo} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet totam iste ratione possimus, pariatur accusamus, ipsum voluptas dolor.</p>
+          </div>
+          <div className="cliente">
+            <img src={logo} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet totam iste ratione possimus, pariatur accusamus, ipsum voluptas dolor.</p>
+          </div>
+          <div className="cliente">
+            <img src={logo} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet totam iste ratione possimus, pariatur accusamus, ipsum voluptas dolor.</p>
+          </div>
+          <div className="cliente">
+            <img src={logo} alt="" />
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Amet totam iste ratione possimus, pariatur accusamus, ipsum voluptas dolor.</p>
+          </div>
+      </div>
+
+      <div className="contactoCont">
+        <h3>Contacto</h3>
+        <img src={logo} alt="" />
+      </div>
     </div>
   )
 }
