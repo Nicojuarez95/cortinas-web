d338b63005ebd58eebf406528984d765c3800aed
Nicolas Juarez <juareznico61@gmail.com>
2024-03-08 09:06:51 -0300

    web cortinas

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -7,7 +7,7 @@ export default function Home() {
   return (
     <div className='home'>
       <h1>VISTA HOME</h1>
-      <Anchor to={'/login'}>Iniciar sesion</Anchor> <br /><Anchor to={'/register'}>Registro</Anchor>
+      {/* <Anchor to={'/login'}>Iniciar sesion</Anchor> <br /><Anchor to={'/register'}>Registro</Anchor> */}
     </div>
   )
 }
